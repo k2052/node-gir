a30501563da4cca087476f319ed46719e8b0be9a
K-2052 <k@2052.me>
2014-01-25 13:21:51 -0800

    fix registration for node 0.10.0. Refs gh-39

--- src/init.h
+++ src/init.h
@@ -5,7 +5,7 @@
 #include <glib.h>
 #include <girepository.h>
 
-v8::Handle<v8::Value> init(const v8::Arguments &args) {
+v8::Handle<v8::Value> g_init(const v8::Arguments &args) {
     v8::HandleScope scope;
     g_type_init();
     return scope.Close(v8::Undefined());
